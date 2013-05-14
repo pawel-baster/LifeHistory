1c5295bea2b16003b51c02a71a959c5b3b0bf06f
pawel-baster <pb@boligportal.dk>
2013-05-14 17:06:29 +0200

    line wrapping in the view

--- view.py
+++ view.py
@@ -9,7 +9,7 @@ class MainWindow( wx.Frame ) :
 		
 		style = (wx.DEFAULT_FRAME_STYLE ^ wx.MINIMIZE_BOX ^ wx.MAXIMIZE_BOX | wx.FRAME_NO_TASKBAR)
 		wx.Frame.__init__(self, None, title='LifeHistory', style = style)
-		self.SetSize( (200, 250) )
+		self.SetSize( (320, 250) )
 		self.SetPosition( (400, 300) )
 		
 		self.panel = wx.Panel(self, wx.ID_ANY)
@@ -30,6 +30,7 @@ class MainWindow( wx.Frame ) :
 		for event in events:
 			year = wx.StaticText(self.panel, label=str(event.startYear))
 			event = wx.StaticText(self.panel, label=event.eventName)
+			event.Wrap(self.GetSize().width - 50)
 			rowSizer = wx.BoxSizer(wx.HORIZONTAL)
 			rowSizer.Add(year, 0, wx.ALL, border=5)
 			rowSizer.Add(event, 0, wx.ALL, border=5)
